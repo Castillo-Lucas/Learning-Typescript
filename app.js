d419ffc22f123931fd04de6843f5a2091b2c8584
Castillo-Lucas <castillolucas182@gmail.com>
2023-06-01 15:43:39 -0300

    Se finaliza seccion

--- app.js
+++ app.js
@@ -13,12 +13,96 @@ let isSuperman = true;
 let isBatman = false;
 isSuperman = isBatman ? false : true; //Si so si hay que asignarle un booleano como reaultado por que si no marca error
 //-- Numbers
+const firstNumber = 10;
+let secondNumber = 20;
 //-- Strings
-//-- Tipo de Any
+//Podemos delcarar strings de las siguientes tres maneras:
+const batman = "Batman";
+const linternaVerde = "Linterna Verde";
+const Ironman = `Ironman`;
+//-- Any
+//Esto sirve para usar que el dato que se usar sea de cualquier tipo. Hay que evitar de usarlo.
+let avenger = "Dr Strange";
+avenger = true; //Luego puede convertirse en un booleano
+avenger = 123; //Y tambien puede convertirse en un number
 //-- Arrays
+//Se pueden definir los tipos de arreglos, estos pueden ser de un solo tipo o combinados.
+const conteo = [1, 2, 3, 4, 5, 6];
+const palabras = ["letra", "casa", "cosa"];
+const convinado = [12, "doce", true];
 //-- Tuples / Tuplas
+//Sirve para determinar el tipo de dato de un arreglo segun la posicion del mismo.
+const hero = [
+    "Spider-man",
+    100,
+    true,
+    "Tela Araña",
+];
+//Luego podemos asignarles otro valor pero respetando lo establecido:
+hero[0] = "Captain America";
+hero[1] = 150;
+hero[2] = false;
+hero[3] = "Mucha fuerza";
 //-- Enum / Enumeraciones
+//Podemos determinar cierto rango de valores:
+var AudioLevel;
+(function (AudioLevel) {
+    AudioLevel[AudioLevel["min"] = 1] = "min";
+    AudioLevel[AudioLevel["medium"] = 5] = "medium";
+    AudioLevel[AudioLevel["max"] = 10] = "max";
+})(AudioLevel || (AudioLevel = {}));
+let currentAudio = AudioLevel.max;
 //-- Void / Vacios
+//TS interpreta que no hay un valor de retorno, lo cual se puede usar para una funcion vacía.
+function batman2() {
+    return;
+}
+const superman = () => {
+    return;
+};
 //-- Never
+//Sirve para determinar una funcion que va a terminar con un error
+const error = (msg) => {
+    if (false) {
+        throw new Error(msg);
+    }
+    return 1;
+};
+error("Auxilio");
 //-- Null
+const incompleto = undefined;
 //-- Undefined
+const nulo = null;
+//--------- TAREA -----
+// Tipos
+const batman3 = "Bruce";
+const superman2 = "Clark";
+const existe = false;
+// Tuplas
+const parejaHeroes = [batman3, superman2];
+const villano = ["Lex Lutor", 5, true];
+// Arreglos
+const aliados = ["Mujer Maravilla", "Acuaman", "San", "Flash"];
+//Enumeraciones
+var PowerLevel;
+(function (PowerLevel) {
+    PowerLevel[PowerLevel["flash"] = 5] = "flash";
+    PowerLevel[PowerLevel["Superman"] = 100] = "Superman";
+    PowerLevel[PowerLevel["Batman"] = 1] = "Batman";
+    PowerLevel[PowerLevel["Acuaman"] = 0] = "Acuaman";
+})(PowerLevel || (PowerLevel = {}));
+const fuerzaFlash = PowerLevel.flash;
+const fuerzaSuperman = PowerLevel.Superman;
+const fuerzaBatman = PowerLevel.Batman;
+const fuerzaAcuaman = PowerLevel.Acuaman;
+// Retorno de funciones
+function activar_batiseñal() {
+    return "activada";
+}
+function pedir_ayuda() {
+    console.log("Auxilio!!!");
+}
+// Aserciones de Tipo
+const poder = "100";
+const largoDelPoder = poder.length;
+console.log(largoDelPoder);
--- app.ts
+++ app.ts
@@ -24,65 +24,111 @@ let secondNumber: number = 20;
 
 //-- Strings
 //Podemos delcarar strings de las siguientes tres maneras:
-const batman: string = 'Batman';
+const batman: string = "Batman";
 const linternaVerde: string = "Linterna Verde";
 const Ironman: string = `Ironman`;
 
 //-- Any
 //Esto sirve para usar que el dato que se usar sea de cualquier tipo. Hay que evitar de usarlo.
-let avenger: any = 'Dr Strange';
+let avenger: any = "Dr Strange";
 avenger = true; //Luego puede convertirse en un booleano
 avenger = 123; //Y tambien puede convertirse en un number
 
 //-- Arrays
 //Se pueden definir los tipos de arreglos, estos pueden ser de un solo tipo o combinados.
 const conteo: number[] = [1, 2, 3, 4, 5, 6];
-const palabras: string[] = ['letra', 'casa', 'cosa'];
-const convinado: (number | string | boolean)[] = [12, 'doce', true]
+const palabras: string[] = ["letra", "casa", "cosa"];
+const convinado: (number | string | boolean)[] = [12, "doce", true];
 
 //-- Tuples / Tuplas
 //Sirve para determinar el tipo de dato de un arreglo segun la posicion del mismo.
-const hero: [string, number, boolean, string] = ['Spider-man', 100, true, 'Tela Araña']
+const hero: [string, number, boolean, string] = [
+  "Spider-man",
+  100,
+  true,
+  "Tela Araña",
+];
 
 //Luego podemos asignarles otro valor pero respetando lo establecido:
-hero[0] = 'Captain America';
+hero[0] = "Captain America";
 hero[1] = 150;
 hero[2] = false;
-hero[3] = 'Mucha fuerza';
+hero[3] = "Mucha fuerza";
 
 //-- Enum / Enumeraciones
 //Podemos determinar cierto rango de valores:
 enum AudioLevel { //Open Upper Case
-    min = 1,
-    medium = 5,
-    max = 10
+  min = 1,
+  medium = 5,
+  max = 10,
 }
 
-let currentAudio = AudioLevel.max
+let currentAudio = AudioLevel.max;
 
 //-- Void / Vacios
 //TS interpreta que no hay un valor de retorno, lo cual se puede usar para una funcion vacía.
-function batman(): void {
-    return;
+function batman2(): void {
+  return;
 }
 
 const superman = (): void => {
-    return;
-}
+  return;
+};
 
 //-- Never
 //Sirve para determinar una funcion que va a terminar con un error
-const error = (msg: string): (never | number) => {
+const error = (msg: string): never | number => {
+  if (false) {
+    throw new Error(msg);
+  }
 
-    if (false) { 
-        throw new Error(msg) 
-    }
+  return 1;
+};
 
-    return 1;
-}
-
-error('Auxilio')
+error("Auxilio");
 
 //-- Null
+const incompleto: undefined = undefined;
 
 //-- Undefined
+const nulo: null = null;
+
+//--------- TAREA -----
+// Tipos
+const batman3: string = "Bruce";
+const superman2: string = "Clark";
+
+const existe: boolean = false;
+
+// Tuplas
+const parejaHeroes: [string, string] = [batman3, superman2];
+const villano: [string, number, boolean] = ["Lex Lutor", 5, true];
+
+// Arreglos
+const aliados: string[] = ["Mujer Maravilla", "Acuaman", "San", "Flash"];
+
+//Enumeraciones
+enum PowerLevel {
+  flash = 5,
+  Superman = 100,
+  Batman = 1,
+  Acuaman = 0,
+}
+const fuerzaFlash = PowerLevel.flash;
+const fuerzaSuperman = PowerLevel.Superman;
+const fuerzaBatman = PowerLevel.Batman;
+const fuerzaAcuaman = PowerLevel.Acuaman;
+
+// Retorno de funciones
+function activar_batiseñal(): string {
+  return "activada";
+}
+
+function pedir_ayuda(): void {
+  console.log("Auxilio!!!");
+}
+
+// Aserciones de Tipo
+const poder: any = "100";
+const largoDelPoder: number = (poder as string).length;
+console.log(largoDelPoder);
